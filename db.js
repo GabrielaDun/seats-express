cac60685ca47357b1045c9494f6bfadc4468a00d
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-08-30 11:31:30 +0200

    Created new enspoints

--- db.js
+++ db.js
@@ -1,4 +1,8 @@
 const db = [
     { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
     { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+    { id: 3, author: 'John Doe', text: 'This company is worth every coin!' },
+    { id: 4, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
 ];
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const db = require('./db');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -11,24 +12,48 @@ app.listen(8000, () => {
 
 
 app.get('/testimonials', (req, res) => {
-    res.render(db);
+    res.json(db);
 }) 
 
-app.get('/testimonialas/:id', (req, res) => {
-    res.json({id: req.params.id });
-})
-
-app.get('/testimonials/random', (req, res) => {
+app.get('/testimonials/randomTwo', (req, res) => {
     const randomIndex = Math.floor(db.length * Math.random())
     res.json(db[randomIndex]);
 })
 
+app.get('/testimonials/:id', (req, res) => {
+
+    const testimonial = db.find(t => t.id === parseInt(req.params.id));
+    res.json(testimonial);
+})
+
 app.post('/testimonials', (req, res) => {
     const { author, text} = req.body;
-    res.json({author, text})
+    res.json({author, text, message: 'OK' })
 })
 
 app.put('/testimonials/:id', (req, res) => {
+    const testimonial = db.find(t => t.id === parseInt (req.params.id));
 
+    if (!testimonial) {
+        return res.status(404).send('Testimonial with a given ID was not found')
+    }
+
+    testimonial.author = req.body.author;
+    testimonial.text = req.body.text;
+
+    res.send({ message: 'OK' })
 })
+
+app.delete('/testimonials/:id', (req, res) => {
+    const index = db.findIndex(t => t.id === parseInt (req.params.id));
+
+    if (index === -1) {
+        return res.status(404).send({ message: 'Testimonial with the given ID was not found.' });
+    }
+
+    const leftTestimonial = db.splice(index, 1);
+
+    res.send({leftTestimonial, message: 'OK'})
+})
+
 
