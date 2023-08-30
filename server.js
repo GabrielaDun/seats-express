a334ef40d849f7183e8244430f42f7cf38638e39
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-08-30 14:11:54 +0200

    Added endpoints for seats and concert data

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const db = require('./db');
+const {testimonials, concerts, seats} = require('./db');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -10,29 +10,25 @@ app.listen(8000, () => {
     console.log('Server is running on port 8000')
 })
 
-
+// Testimonials
 app.get('/testimonials', (req, res) => {
-    res.json(db);
+    res.json(testimonials);
 }) 
-
 app.get('/testimonials/randomTwo', (req, res) => {
-    const randomIndex = Math.floor(db.length * Math.random())
-    res.json(db[randomIndex]);
+    const randomIndex = Math.floor(testimonials.length * Math.random())
+    res.json(testimonials[randomIndex]);
 })
-
 app.get('/testimonials/:id', (req, res) => {
 
-    const testimonial = db.find(t => t.id === parseInt(req.params.id));
+    const testimonial = testimonial.find(t => t.id === parseInt(req.params.id));
     res.json(testimonial);
 })
-
 app.post('/testimonials', (req, res) => {
     const { author, text} = req.body;
     res.json({author, text, message: 'OK' })
 })
-
 app.put('/testimonials/:id', (req, res) => {
-    const testimonial = db.find(t => t.id === parseInt (req.params.id));
+    const testimonial = testimonial.find(t => t.id === parseInt (req.params.id));
 
     if (!testimonial) {
         return res.status(404).send('Testimonial with a given ID was not found')
@@ -43,17 +39,108 @@ app.put('/testimonials/:id', (req, res) => {
 
     res.send({ message: 'OK' })
 })
-
 app.delete('/testimonials/:id', (req, res) => {
-    const index = db.findIndex(t => t.id === parseInt (req.params.id));
+    const index = testimonials.findIndex(t => t.id === parseInt (req.params.id));
 
     if (index === -1) {
         return res.status(404).send({ message: 'Testimonial with the given ID was not found.' });
     }
 
-    const leftTestimonial = db.splice(index, 1);
+    const leftTestimonial = testimonials.splice(index, 1);
 
     res.send({leftTestimonial, message: 'OK'})
 })
 
+// Concert
+
+app.get('/concerts', (req, res) => {
+    res.json(concerts)
+})
+
+app.get('/concerts/:id', (req, res) => {
+    const concert = concerts.find(f => f.id === req.params.id)
+
+    res.json(concert);
+})
+
+app.post('/concerts', (req, res) => {
+    const { author, text } = req.body
+    res.json({author, text, message: 'ok'})
+})
+
+app.put('/concerts', (req, res) => {
+    const concert = concerts.find(c => c.id === req.params.id)
+
+    if (!concert) {
+        return res.status(404).send({message: 'Concert with the givern ID was not found'})
+    }
+
+    concert.author = req.body.author
+    concert.text = req.body.text
+
+    res.send({message: 'OK'})
+
+})
+
+app.delete('/concerts/:id', (req, res) => {
+    const index = concerts.findIndex(c => c.id === req.params.id)
+
+    if (index === -1) {
+        return res.status(404).send({message: 'Concert with given ID was not found'})
+    }
+    const leftConcerts = seats.splice(index, 1)
+
+    res.json({messgae: ok, leftConcerts})
+})
+
+
+// Seats
+
+app.get('/seats', (req, res) => {
+    res.json(seats)
+})
+
+app.get('/seats/:id', (req, res) => {
+    const seat = seats.find(s => s.id === parseInt(req.params.id))
+    res.json(seat)
+})
+
+app.post('/seats', (req, res) => {
+    const {author, text} = req.body;
+    res.json( {author, text})
+})
+
+app.put('/seats/:id', (req, res) => {
+    const seat = seats.find( s => s.id === parseInt(req.params.id))
+
+    if (!seat) {
+        return res.status(404).send({ message: 'Seat with the given ID was not found'})
+    }
+
+    seat.author = req.body.author
+    seat.text = req.body.text
+
+    res.send({message: 'OK'})
+})
+
+app.delete('/seats/:id', (req, res) => {
+    const index = seats.findIndex( s => s.id === parseInt(req.params.id))
+
+    if (index === -1) {
+        return res.status(404).send({ message: 'Seat with given ID was not found'})
+    }
+
+    const leftSeat = seats.splice(index, 1)
+
+    res.send({message: OK, leftSeat})
+
+})
+
+
+
+
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+})
 
