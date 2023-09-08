34e06bec15cd1aed0ce86d536bbe726b5bea0cb9
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-09-08 07:58:17 +0200

    Prepared for publishing

--- server.js
+++ server.js
@@ -13,10 +13,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, '/client/build')));
 app.use('/api', testimonialsRouter);
 app.use('/api', concertsRouter);
 app.use('/api', seatsRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
